6eaf201a9d2b66d12ed4749e1d9930db4ed3b97d
Plawn <plawn.yay@gmail.com>
2018-12-07 01:19:20 +0100

    a

--- controller/meteto_req.js
+++
@@ -1,9 +0,0 @@
-// medical endpoint
-const meteo = require('../models/meteo.js');
-
-
-var getAllMedical = (db, req, res) => {
-    res.send(medical.getAll(db))
-}
-
-module.exports.getAllMeteo = getAllMeteo;
--- models/position.js
+++ models/position.js
@@ -1,15 +1,15 @@
 const uuid = require('uuid4');
 
 const getAll = (con, callback) => {
-    con.query("SELECT * FROM Postion", function (err, result, fields) {
+    con.query("SELECT * FROM Position", function (err, result, fields) {
         if (err) throw err;
-        return result;
+        callback(result);
     });
 }
 const getById = (con, id, callback) => {
-    con.query("SELECT * FROM Postion WHERE ID = ?", [id], function (err, result, fields) {
+    con.query("SELECT * FROM Position WHERE ID = ?", [id], function (err, result, fields) {
         if (err) throw err;
-        return result;
+        callback(result);
     });
 }
 const add = (con, longitude, latitude, callback) => {

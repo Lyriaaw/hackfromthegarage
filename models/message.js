f685486dc1cb18172d0782a22d158e1a85072824
Plawn <plawn.yay@gmail.com>
2018-12-07 02:17:34 +0100

    a

--- models/message.js
+++ models/message.js
@@ -1,27 +1,24 @@
 var uuid4 = require('uuid4');
 
-function addMessage(con, text, timestamp, image, sender) {
+function addMessage(con, text, timestamp, image, sender, callback) {
     var uuid = uuid4()
     con.query("INSERT INTO Message (UID, texte, timestamp, image, sender) VALUES (?, ?, ?, ?, ?)", [uuid, text, timestamp, image, sender], function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        return result;
+        if (err) callback("");
+        callback(result);
       });
 }
 
 function getByID(con, id) {
     con.query("SELECT * FROM Message WHERE ID=?", [id], function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        return result;
+        if (err) callback("");
+        callback(result);
       });
 }
 
 function getAll(con) {
     con.query("SELECT * FROM Message", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        return result;
+        if (err) callback("");
+        callback(result);
       });
 }
 

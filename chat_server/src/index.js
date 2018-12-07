e52f5a6d8a73b518d8054f09d55b2ef455c5332d
Amalric de Buffières <amalric.debuffieres@gmail.com>
2018-12-07 01:50:52 +0100

    Added chat server'

---
+++ chat_server/src/index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', function (socket) {
+    console.log('New user');
+    socket.on('message', function (message) {
+        console.log('message', message);
+        io.emit('message', message);
+    });
+    socket.on('disconnect', function () {
+        console.log('User disconected');
+    });
+});
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
